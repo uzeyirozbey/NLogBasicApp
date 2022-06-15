b96eeba0d64d4293dcfcd0897bee3e931e19e9e4
uzeyirozbey <41063849+uzeyirozbey@users.noreply.github.com>
2022-06-15 17:37:41 +0300

    NLog  Kullanımı
    NLog  Kullanımı

---
+++ LogUygulama/Program.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log logT = new Log();
+            logT.StartTransaction();
+            logT.DoSomething();
+            logT.StopTransaction();
+            Console.ReadLine();
+        }
+        class Log
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            public void StartTransaction()
+            {
+                logger.Info("Transaction başlatıldı.");
+            }
+            public void DoSomething()
+            {
+                logger.Fatal("servis çalışmıyor");
+                logger.Error("db bağlantısı sağlanamadı.");
+            }
+            public void StopTransaction()
+            {
+                logger.Warn("Transaction sonlandı");
+            }
+        }
+    }
+}
